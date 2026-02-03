6c197e5baeafd694b5e10ad91e2533c42a1a7545
ronaldod1 <ronaldod@housecricket.com.br>
2026-02-03 13:48:36 -0300

    ajuste hosts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,15 +1,31 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
-  },
-  server: {
-    headers: {
-      "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    },
-  },
+	plugins: [react()],
+	optimizeDeps: {
+		exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+	},
+	// Configuração para Desenvolvimento Local (npm run dev)
+	server: {
+		headers: {
+			"Cross-Origin-Embedder-Policy": "require-corp",
+			"Cross-Origin-Opener-Policy": "same-origin",
+		},
+	},
+	// Configuração para Produção no Coolify (npm run preview)
+	preview: {
+		host: true, // Libera o acesso externo (0.0.0.0)
+		port: 3000, // Garante a porta 3000
+		allowedHosts: [
+			// AQUI ESTÁ A CORREÇÃO DO BLOQUEIO
+			"legendas.housecricket.com.br",
+			"www.legendas.housecricket.com.br",
+		],
+		headers: {
+			// IMPORTANTE: Repetir os headers aqui para o FFmpeg funcionar em produção
+			"Cross-Origin-Embedder-Policy": "require-corp",
+			"Cross-Origin-Opener-Policy": "same-origin",
+		},
+	},
 });
